a1f8eb37fff54ed743a95a48610cbbeb23f9d0f2
Colin Matteson <seefm@proton.me>
2023-12-04 19:03:52 -0500

    refactored app to reduce redundancies in code, checking with instructors on this tomorrow to see if my approach was sound or not

--- Program.cs
+++ Program.cs
@@ -65,50 +65,69 @@ void MooseSays(string message)
 
 MooseSays("WHAT THE FUUUUUUUUUUUUUUUUUCK IN A GOOD WAY THO I'M NOT MAD I'M JUST ENTHUSIASTIC");
 
-
-
-
-void CanadaQuestion ()
+void Questions () 
 {
-    bool isTrue = MooseAsks("Is Canada real?");
-    if (isTrue)
-    {
-        MooseSays("Really? It seems very unlikely.");
-    }
-    else
-    {
-        MooseSays("I  K N E W  I T !!!");
-    }
+    List<string> AIQuestions = new List<string>() { "Is Canada real?", "You like food tho?", "You got a car or are you out here strugglin?"};
+    List<string> TrueAnswers = new List<string>() { "Really? It seems very unlikely", "This is the only correct answer", "damn ok I see you mr. monopoly money bags MF"};
+    List<string> FalseAnswers = new List<string>() { "I K N E W I T!!!!!!!", "wtf who hurt you bro?", "ouch it's like they say the strugg is rill"};
+
+    for (int i = 0; i < 3; i++)
+        {
+           bool isTrue = MooseAsks(AIQuestions[i]);
+           if (isTrue)
+           {
+            MooseSays(TrueAnswers[i]);
+           }
+           else 
+           {
+            MooseSays(FalseAnswers[i]);
+           }
+        }
+
 }
 
-void FoodQuestion ()
-{
-    bool isTrue = MooseAsks("You like food tho?");
-    if (isTrue)
-    {
-        MooseSays("this is the only correct answer");
-    }
-    else
-    {
-        MooseSays("wtf who hurt you bro");
-    }
-}
-
-void CarQuestion ()
-{
-    bool isTrue = MooseAsks("You got a car or are you out here strugglin?");
-    if (isTrue)
-    {
-        MooseSays("damn ok flex mr. monopoly money bag mf");
-    }
-    else
-    {
-        MooseSays("ouch");
-    }
-}
-
-CanadaQuestion();
-FoodQuestion();
-CarQuestion();
+//void CanadaQuestion ()
+//{
+//    bool isTrue = MooseAsks("Is Canada real?");
+//    if (isTrue)
+//    {
+//        MooseSays("Really? It seems very unlikely.");
+//    }
+//    else
+//    {
+//        MooseSays("I  K N E W  I T !!!");
+//    }
+//}
+//
+//void FoodQuestion ()
+//{
+//    bool isTrue = MooseAsks("You like food tho?");
+//    if (isTrue)
+//    {
+//        MooseSays("this is the only correct answer");
+//    }
+//    else
+//    {
+//        MooseSays("wtf who hurt you bro");
+//    }
+//}
+//
+//void CarQuestion ()
+//{
+//    bool isTrue = MooseAsks("You got a car or are you out here strugglin?");
+//    if (isTrue)
+//    {
+//        MooseSays("damn ok flex mr. monopoly money bag mf");
+//    }
+//    else
+//    {
+//        MooseSays("ouch");
+//    }
+//}
+//
+//CanadaQuestion();
+//FoodQuestion();
+//CarQuestion();
+Questions();
 
 }
